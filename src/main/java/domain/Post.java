4a80ee16ad196a2ca1458240cae2107b395dd44a
user <test@>
2021-11-09 23:24:46 +0300

    Добавлен файл .gitignore, текст программы Post

---
+++ src/main/java/domain/Post.java
@@ -0,0 +1,17 @@
+package domain;
+
+public class Post {
+    private int id; //id записи
+    private String name; //имя публикатора
+    private String LogoUrl; // ссылка на изображение отправителя записи
+    private int date; //дата публикации
+    private String text; //запись в посте
+    private LikesInfo likesInfo; // Лайки
+    private RepostsInfo repostsInfo; // репосты
+    private CommentsInfo commentsInfo; //комментарии
+    private ViewsInfo viewsInfo;// количиество просмотров
+
+    //get+set
+
+
+}
